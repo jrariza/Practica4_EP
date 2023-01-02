92000d7cc44e72863c3a11afdd31b4b7b8951cd4
arnau <arnaurodriguezgonzalez@gmail.com>
2023-01-02 13:16:42 +0100

    CrimConviction + Tests passed

---
+++ src/publicadministration/CrimConviction.java
@@ -0,0 +1,37 @@
+package publicadministration;
+
+import java.util.Date;
+
+public class CrimConviction {
+    // Represents a criminal conviction registered
+    private Date commitDate;
+    private String offense;
+    private String sentence;
+
+    public CrimConviction(Date commit, String off, String sentc) {
+        this.commitDate = commit;
+        this.offense = off;
+        this.sentence = sentc;
+    }
+
+    public Date getCommitDate() {
+        return commitDate;
+    }
+
+    public String getOffense() {
+        return offense;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    @Override
+    public String toString() {
+        return "CrimConviction{" +
+                "commitDate=" + commitDate +
+                ", offense='" + offense + '\'' +
+                ", sentence='" + sentence + '\'' +
+                '}';
+    }
+}
