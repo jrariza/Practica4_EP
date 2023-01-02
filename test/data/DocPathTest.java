5dedd06fef06a1b1a4561ce161db9397b00bcd60
arnau <arnaurodriguezgonzalez@gmail.com>
2023-01-02 12:31:17 +0100

    DocPath + Test

---
+++ test/data/DocPathTest.java
@@ -0,0 +1,17 @@
+package data;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DocPathTest {
+
+    @Test
+    public void samePath() throws Exception {
+        String tmp_path = "file.txt";
+
+        DocPath tmp_d = new DocPath(tmp_path);
+
+        Assertions.assertEquals(tmp_path, tmp_d.getDocPath());
+    }
+
+}
